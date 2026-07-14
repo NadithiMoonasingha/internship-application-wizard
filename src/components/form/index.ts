5e820e885e9ddf77d9021b0df0ed2c8f328a6849
Nadithi <moonasinghanadithi@gmail.com>
2026-07-14 21:51:08 +0530

    Created the component export file

---
+++ src/components/form/index.ts
@@ -0,0 +1,5 @@
+export { default as FormField } from "./FormField";
+export { default as SelectInput } from "./SelectInput";
+export { default as TextInput } from "./TextInput";
+
+export type { SelectOption } from "./SelectInput";

61b65fc8ea810d0b9f001183c0abda7e60d6cedc
Nadithi <moonasinghanadithi@gmail.com>
2026-07-17 11:10:35 +0530

    Exported FileInput

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,3 +1,4 @@
+export { default as FileInput } from "./FileInput";
 export { default as FormField } from "./FormField";
 export { default as SelectInput } from "./SelectInput";
 export { default as TextInput } from "./TextInput";
